2e6a231e6a00c3016ffdddf748c4dc6962ed201a
chebilyoussef21 <youssefchebil@vt.edu>
2026-03-24 21:03:51 -0400

    changed scheduler.h error for DMS_POLICY macro AND added code to project3 ino file

--- project3_RTS/project3_RTS.ino
+++ project3_RTS/project3_RTS.ino
@@ -2,25 +2,36 @@
 
 TaskHandle_t xHandle1 = NULL;
 TaskHandle_t xHandle2 = NULL;
+TaskHandle_t xHandle3 = NULL;
+TaskHandle_t xHandle4 = NULL;
 
 static void testFunc1( void *pvParameters )
 {
 	(void) pvParameters;
-	volatile int i,a;
-	for( i = 0; i < 10000; i++ )
-	{
-		a = 1 + i*i*i*i;
-	}	
+	Serial.println("task 1");
 }
 
 static void testFunc2( void *pvParameters )
 { 
 	(void) pvParameters;	
-	volatile int i, a;	
-	for(i = 0; i < 10000; i++ )
-	{
-		a = 1 + a * a * i;
-	}
+	Serial.println("task 2");
+}
+
+static void testFunc3( void *pvParameters )
+{
+	(void) pvParameters;
+	Serial.println("task 3");
+}
+
+static void testFunc4( void *pvParameters )
+{ 
+	(void) pvParameters;	
+	Serial.println("task 4");
+	// volatile int i, a;	
+	// for(i = 0; i < 10000; i++ )
+	// {
+	// 	a = 1 + a * a * i;
+	// }
 }
 
 void setup()
@@ -32,13 +43,22 @@ void setup()
   }
 	char c1 = 'a';
 	char c2 = 'b';			
+	char c3 = 'c';
+	char c4 = 'd';		
 
 	vSchedulerInit();
 
-	vSchedulerPeriodicTaskCreate(testFunc1, "t1", configMINIMAL_STACK_SIZE, &c1, 1, &xHandle1, pdMS_TO_TICKS(0), pdMS_TO_TICKS(800), pdMS_TO_TICKS(100), pdMS_TO_TICKS(800));
-	vSchedulerPeriodicTaskCreate(testFunc2, "t2", configMINIMAL_STACK_SIZE, &c2, 2, &xHandle2, pdMS_TO_TICKS(0), pdMS_TO_TICKS(400), pdMS_TO_TICKS(400), pdMS_TO_TICKS(400));
+	//task set 1
+	vSchedulerPeriodicTaskCreate(testFunc1, "t1", configMINIMAL_STACK_SIZE, &c1, 1, &xHandle1, pdMS_TO_TICKS(0), pdMS_TO_TICKS(400), pdMS_TO_TICKS(100), pdMS_TO_TICKS(400));
+	vSchedulerPeriodicTaskCreate(testFunc2, "t2", configMINIMAL_STACK_SIZE, &c2, 2, &xHandle2, pdMS_TO_TICKS(0), pdMS_TO_TICKS(800), pdMS_TO_TICKS(200), pdMS_TO_TICKS(800));
+	vSchedulerPeriodicTaskCreate(testFunc3, "t3", configMINIMAL_STACK_SIZE, &c3, 3, &xHandle3, pdMS_TO_TICKS(0), pdMS_TO_TICKS(1000), pdMS_TO_TICKS(150), pdMS_TO_TICKS(1000));
+	vSchedulerPeriodicTaskCreate(testFunc4, "t4", configMINIMAL_STACK_SIZE, &c4, 4, &xHandle4, pdMS_TO_TICKS(0), pdMS_TO_TICKS(5000), pdMS_TO_TICKS(300), pdMS_TO_TICKS(5000));
 
-
+	//task set 2
+	// vSchedulerPeriodicTaskCreate(testFunc1, "t1", configMINIMAL_STACK_SIZE, &c1, 1, &xHandle1, pdMS_TO_TICKS(0), pdMS_TO_TICKS(400), pdMS_TO_TICKS(100), pdMS_TO_TICKS(400));
+	// vSchedulerPeriodicTaskCreate(testFunc2, "t2", configMINIMAL_STACK_SIZE, &c2, 2, &xHandle2, pdMS_TO_TICKS(0), pdMS_TO_TICKS(200), pdMS_TO_TICKS(150), pdMS_TO_TICKS(200));
+	// vSchedulerPeriodicTaskCreate(testFunc1, "t3", configMINIMAL_STACK_SIZE, &c3, 3, &xHandle3, pdMS_TO_TICKS(0), pdMS_TO_TICKS(800), pdMS_TO_TICKS(200), pdMS_TO_TICKS(700));
+	// vSchedulerPeriodicTaskCreate(testFunc2, "t4", configMINIMAL_STACK_SIZE, &c4, 4, &xHandle4, pdMS_TO_TICKS(0), pdMS_TO_TICKS(1000), pdMS_TO_TICKS(150), pdMS_TO_TICKS(1000));
 
 	vSchedulerStart();
 
